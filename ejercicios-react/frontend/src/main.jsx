452ef12d39e7f243537184df6780bb490efefda0
unknown <Jis12102005@gmail.com>
2026-01-08 12:36:47 -0300

    modificacion

--- ejercicios-react/frontend/src/main.jsx
+++ ejercicios-react/frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Login from './Login.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router";
+import './index.css';
+import Login from './Login.jsx';
+import Activities from './Activities.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element = {<Login />} />
+      <Route path='/activities' element = {<Activities />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
