196458ec441cda77a4f091e503f7461a6aeeb430
unknown <Jis12102005@gmail.com>
2026-01-08 16:22:02 -0300

    modificacion

--- ejercicios-react/frontend/src/pages/Home.jsx
+++ ejercicios-react/frontend/src/pages/Home.jsx
@@ -1,6 +1,29 @@
+import "@styles/Home.css";
+import yogaImg from "../assets/home/yoga.webp";
+import zumbaImg from "../assets/home/zumba.jpg";
+
+
+
 const Home = () => {
     return(
-        <h2>Informacion del GYM</h2>
+        <div className="card-home">
+          <div>
+            <img src="../../public/home/funcional.jpg" alt="Funcional" />
+            <h2>Funcional</h2>
+          </div>
+          <div>
+            <img src="../../public/home/taekwondo.jpg" alt="Taekwondo" />
+            <h2>Taekwondo</h2>
+          </div>
+          <div>
+            <img src={yogaImg} alt="Yoga" />
+            <h2>Yoga</h2>
+          </div>
+          <div>
+            <img src= {zumbaImg} alt="Zumba" />
+            <h2>Zumba</h2>
+          </div>
+        </div>
     )
 }
 
